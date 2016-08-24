8f199fe06ec87431555fec1f6a5ddc7e288c42f1
qiao1406 <980898832@qq.com>
2016-08-25 06:08:17 +0800

    v2.0

--- src/Calculator/CalFrame.java
+++ src/Calculator/CalFrame.java
@@ -2,7 +2,9 @@ package Calculator;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,9 +21,10 @@ public class CalFrame extends JFrame {
 	
 	//配色
 	private final static Color OP_COLOR = new Color(251, 150, 110);
-	private final static Color NUM_COLOR = new Color(36, 147, 110);
+	private final static Color NUM_COLOR = new Color(36, 147, 190);
 	private final static Color EQUAL_COLOR = new Color(239, 187, 36);
-	private final static Color CLR_COLOR = new Color(0, 92, 175);
+	private final static Color CLR_COLOR = new Color(50, 252, 75);
+	private final static Color DEL_COLOR = new Color(0, 152, 120);
 	
 	private final static Font FONT1 = new Font("黑体", Font.BOLD, 20);
 	
@@ -35,22 +38,29 @@ public class CalFrame extends JFrame {
 	private JButton num7 = new JButton("7");
 	private JButton num8 = new JButton("8");
 	private JButton num9 = new JButton("9");
+	private JButton decimalPoint = new JButton(".");
 	private JButton addButton = new JButton("+");
 	private JButton minusButton = new JButton("-");
 	private JButton mulButton = new JButton("*");
 	private JButton divButton = new JButton("/");
 	private JButton equalButton = new JButton("=");
+	private JButton leftBracket = new JButton("(");
+	private JButton rightBracket = new JButton(")");
 	private JButton clearButton = new JButton("Clear");
+	private JButton deleteButton = new JButton("Del");
 	
-	private JLabel text = new JLabel("result");
-	private JTextArea rst = new JTextArea(1,30);
+	private JLabel equationLabel = new JLabel("Equation");
+	private JLabel resultLabel = new JLabel("Result");
+	private JTextArea equation = new JTextArea(1,30);
+	private JTextArea result = new JTextArea(1,30);
 
 	private JPanel jp1 = new JPanel();
 	private JPanel jp2 = new JPanel();
 
 	public CalFrame() {
 
-		rst.setEditable(false);
+		equation.setEditable(false);
+		result.setEditable(false);
 		
 		//颜色,字体设置
 		num0.setBackground(NUM_COLOR);
@@ -63,6 +73,8 @@ public class CalFrame extends JFrame {
 		num7.setBackground(NUM_COLOR);
 		num8.setBackground(NUM_COLOR);
 		num9.setBackground(NUM_COLOR);
+		decimalPoint.setBackground(NUM_COLOR);
+		
 		addButton.setBackground(OP_COLOR);
 		addButton.setFont(FONT1);
 		minusButton.setBackground(OP_COLOR);
@@ -71,9 +83,14 @@ public class CalFrame extends JFrame {
 		mulButton.setFont(FONT1);
 		divButton.setBackground(OP_COLOR);
 		divButton.setFont(FONT1);
+		leftBracket.setBackground(OP_COLOR);
+		leftBracket.setFont(FONT1);
+		rightBracket.setBackground(OP_COLOR);
+		rightBracket.setFont(FONT1);
 		equalButton.setBackground(EQUAL_COLOR);
 		equalButton.setFont(FONT1);
 		clearButton.setBackground(CLR_COLOR);
+		deleteButton.setBackground(DEL_COLOR);
 		
 		
 		//添加动作
@@ -81,149 +98,204 @@ public class CalFrame extends JFrame {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				rst.setText(rst.getText() + "0");
+				equation.setText(equation.getText() + "0");
 			}
 		});
 		num1.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				rst.setText(rst.getText() + "1");
+				equation.setText(equation.getText() + "1");
 			}
 		});
 		num2.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				rst.setText(rst.getText() + "2");
+				equation.setText(equation.getText() + "2");
 			}
 		});
 		num3.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				rst.setText(rst.getText() + "3");
+				equation.setText(equation.getText() + "3");
 			}
 		});
 		num4.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				rst.setText(rst.getText() + "4");
+				equation.setText(equation.getText() + "4");
 			}
 		});
 		num5.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				rst.setText(rst.getText() + "5");
+				equation.setText(equation.getText() + "5");
 			}
 		});
 		num6.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				rst.setText(rst.getText() + "6");
+				equation.setText(equation.getText() + "6");
 			}
 		});
 		num7.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				rst.setText(rst.getText() + "7");
+				equation.setText(equation.getText() + "7");
 			}
 		});
 		num8.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				rst.setText(rst.getText() + "8");
+				equation.setText(equation.getText() + "8");
 			}
 		});
 		num9.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				rst.setText(rst.getText() + "9");
+				equation.setText(equation.getText() + "9");
+			}
+		});
+		decimalPoint.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				equation.setText(equation.getText() + ".");
 			}
 		});
 		addButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				rst.setText(rst.getText() + "+");
+				equation.setText(equation.getText() + "+");
 			}
 		});
 		minusButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				rst.setText(rst.getText() + "-");
+				equation.setText(equation.getText() + "-");
 			}
 		});
 		mulButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				rst.setText(rst.getText() + "*");
+				equation.setText(equation.getText() + "*");
 			}
 		});
 		divButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				rst.setText(rst.getText() + "/");
+				equation.setText(equation.getText() + "/");
+			}
+		});
+		leftBracket.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				equation.setText(equation.getText() + "(");
+			}
+		});
+		rightBracket.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				equation.setText(equation.getText() + ")");
 			}
 		});
 		equalButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String cmd = rst.getText();
-				rst.setText(Calculate.calResult(cmd));
+				String cmd = equation.getText();
+				Calculate cl = new Calculate();
+				result.setText(cl.calResult(cmd));
 			}
 		});
 		clearButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				rst.setText("");
+				equation.setText("");
+				result.setText("");
+			}
+		});
+		deleteButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if( !( equation.getText().equals("")) ) {
+					StringBuffer sb = new StringBuffer();
+					sb.append(equation.getText());
+					sb.delete(sb.length()-1 , sb.length());
+					equation.setText(sb.toString());
+				}
 			}
 		});
 		
-		jp1.add(text);
-		jp1.add(rst);
+//		jp1.setLayout(null);
+		jp1.add(equationLabel);
+		jp1.add(equation);
+		jp1.add(resultLabel);
+		jp1.add(result);
+		equationLabel.setBounds(0, 0, 50, 30);
+		equationLabel.setLocation(0, 0);
+		equation.setBounds(50, 0, 150, 30);
+		equation.setLocation(50, 0);
+		resultLabel.setBounds(0, 30, 50, 30);
+		resultLabel.setLocation(0, 30);
+		result.setBounds(50, 30, 150, 30);
+		result.setLocation(50, 30);
 
-		jp2.setLayout(new GridLayout(4, 4));
+		jp2.setLayout(new GridLayout(4, 5));
 		// line-1
 		jp2.add(num7);
 		jp2.add(num8);
 		jp2.add(num9);
 		jp2.add(addButton);
+		jp2.add(leftBracket);
 		// line-2
 		jp2.add(num4);
 		jp2.add(num5);
 		jp2.add(num6);
 		jp2.add(minusButton);
+		jp2.add(rightBracket);
 		// line-3
 		jp2.add(num1);
 		jp2.add(num2);
 		jp2.add(num3);
 		jp2.add(mulButton);
+		jp2.add(clearButton);
 		// line-4
 		jp2.add(num0);
+		jp2.add(decimalPoint);
 		jp2.add(equalButton);
-		jp2.add(clearButton);
 		jp2.add(divButton);
+		jp2.add(deleteButton);
 		
 		jp1.setLocation(0, 0);
+		jp1.setVisible(true);
 		jp2.setLocation(0, 100);
+		jp2.setVisible(true);
 		
-		add(BorderLayout.NORTH, jp1);
-		add(BorderLayout.SOUTH, jp2);
+		setLayout( new GridLayout(2,1));
+//		add(BorderLayout.NORTH, jp1);
+//		add(BorderLayout.SOUTH, jp2);
+		add(jp1);
+		add(jp2);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("QiaoCalculator v1.2-稳定版");
-		setSize(400, 400);
+		setTitle("QiaoCalculator v2.0");
+		setSize(430, 400);
 		setLocation(200, 200);
 		setVisible(true);
 	}
--- src/Calculator/Calculate.java
+++ src/Calculator/Calculate.java
@@ -1,75 +1,127 @@
 package Calculator;
 
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculate {
 	
-	public static String calResult ( String cmd ) {
-		String regex = "\\d+[\\+\\-\\*\\/]\\d+";
-		Pattern p = Pattern.compile(regex);
-		Matcher m = p.matcher(cmd);
-		if ( m.find() ) {
+	private Stack<Double> numStack = new Stack<Double>();
+	private Stack<Character> sybStack = new Stack<Character>(); 
+	
+	public String calResult ( String cmd ) {
+		cmd += "#";
+		
+		StringBuffer tempNum = new StringBuffer();
+		StringBuffer exp = new StringBuffer().append(cmd);
+		
+		while ( exp.length() != 0 ) {
 			
-			String opRegex = "[\\+\\-\\*\\/]";
-			String[] s = Pattern.compile(opRegex).split(cmd);
-			Matcher m1 = Pattern.compile(opRegex).matcher(cmd);
+			String temp = exp.substring(0,1);
+			exp.delete(0, 1);
 			
-			int num1 = Integer.parseInt(s[0]);
-			int num2 = Integer.parseInt(s[1]);
-			m1.find();
-			String op = String.valueOf( cmd.charAt( m1.start() ) );
-			String result = cal(num1, num2, op);
-			return cmd + " = " + result;
+			if( !isNum(temp) ) { // temp不是数字
+				if (!"".equals(tempNum.toString())) {
+					// 当表达式的第一个符号为括号
+					double num = Double.parseDouble(tempNum.toString());
+					numStack.push(num);
+					tempNum.delete(0, tempNum.length());
+				}
+				// 用当前取得的运算符与栈顶运算符比较优先级：若高于，则因为会先运算，放入栈顶；若等于，因为出现在后面，所以会后计算，所以栈顶元素出栈，取出操作数运算；
+				// 若小于，则同理，取出栈顶元素运算，将结果入操作数栈。
+
+				// 判断当前运算符与栈顶元素优先级，取出元素，进行计算(因为优先级可能小于栈顶元素，还小于第二个元素等等，需要用循环判断)
+				while ( !compare(temp.charAt(0)) && (!sybStack.empty()) ) {
+					double a = numStack.pop();
+					double b = numStack.pop();
+					char ope = sybStack.pop();
+					double result = 0;
+					
+					switch (ope) {
+					// 如果是加号或者减号，则
+					case '+':
+						result = b + a;
+						// 将操作结果放入操作数栈
+						numStack.push(result);
+						break;
+					case '-':
+						result = b - a;
+						// 将操作结果放入操作数栈
+						numStack.push(result);
+						break;
+					case '*':
+						result = b * a;
+						// 将操作结果放入操作数栈
+						numStack.push(result);
+						break;
+					case '/':
+						result = b / a;// 将操作结果放入操作数栈
+						numStack.push(result);
+						break;
+					}
+
+				}
+				
+				// 判断当前运算符与栈顶元素优先级， 如果高，或者低于平，计算完后，将当前操作符号，放入操作符栈
+				if (temp.charAt(0) != '#') {
+					sybStack.push(new Character(temp.charAt(0)));
+					if (temp.charAt(0) == ')') {// 当栈顶为'('，而当前元素为')'时，则是括号内以算完，去掉括号
+						sybStack.pop();
+						sybStack.pop();
+					}
+				}
+			}
+			else { // temp是数字
+				tempNum.append(temp);
+			}
+			
 		}
-		else {
-			return "输入错误";
-		}
+		return numStack.pop() + "";
 	}
 
-	public static String cal(int n1, int n2, String op) {
-
-		String resultStr = "";
-		int resultInt;
-		double resultDouble;
-
-		switch (op) {
-		case "+":
-			resultInt = n1 + n2;
-			resultStr = String.valueOf(resultInt);
-			break;
-
-		case "-":
-			resultInt = n1 - n2;
-			resultStr = String.valueOf(resultInt);
-			break;
-
-		case "*":
-			resultInt = n1 * n2;
-			resultStr = String.valueOf(resultInt);
-			break;
-
-		case "/":
-			
-			if( n2 == 0 ) {
-				resultStr = "错误，除数不能为0";
-			}
-			else {
-				double d1 = (double)n1;
-				double d2 = (double)n2;
-				resultDouble = d1/d2;
-				resultStr = String.valueOf(resultDouble);
-			}
-			
-			
-			break;
-
-		default:
-			resultStr = "Unknown Error";
-			break;
-		}
-
-		return resultStr;
+	private static boolean isNum(String temp) {
+		return temp.matches("[0-9]") || temp.equals(".");
 	}
 
+	private boolean compare(char str) {
+		if ( sybStack.empty() ) {
+			// 当为空时，显然 当前优先级最低，返回高
+			return true;
+		}
+		char last = (char) sybStack.lastElement();
+		// 如果栈顶为'('显然，优先级最低，')'不可能为栈顶。
+		if (last == '(') {
+			return true;
+		}
+		switch (str) {
+		case '#':
+			return false;// 结束符
+		case '(':
+			// '('优先级最高,显然返回true
+			return true;
+		case ')':
+			// ')'优先级最低，
+			return false;
+		case '*': {
+			// '*/'优先级只比'+-'高
+			if (last == '+' || last == '-')
+				return true;
+			else
+				return false;
+		}
+		case '/': {
+			if (last == '+' || last == '-')
+				return true;
+			else
+				return false;
+		}
+		// '+-'为最低，一直返回false
+		case '+':
+			return false;
+		case '-':
+			return false;
+		}
+		return true;
+	}
+	
 }
